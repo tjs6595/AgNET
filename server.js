60d8a757db575d37c14a73e668e296fbbb202985
tjs6595 <t.schultejans@schenckprocess.com>
2022-10-11 00:14:17 -0500

    Added Default view, Home view, and style.css for styling.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
+const methodOverride = require('method-override')
 
 
 // CONFIGURATION / MIDDLEWARE
@@ -10,11 +11,21 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+
 // ROOT
 app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to the AgNET Home Page'
-    })
+    // res.status(200).json({
+    //     message: 'Welcome to the AgNET Home Page about Breads'
+    // })
+    res.render('home')
 })
 
 
