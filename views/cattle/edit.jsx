3910ddaf101804ec6f085600e1ac89e2efe52f21
tjs6595 <t.schultejans@schenckprocess.com>
2022-10-30 21:25:56 -0500

    Added unsaved file.

--- views/cattle/edit.jsx
+++ views/cattle/edit.jsx
@@ -13,22 +13,3 @@ function editForm (data) {
 }
 
 module.exports = editForm
-
-
-
-// <form method='POST' action={`./?_method=PUT`}>
-// <div className='form-group'>
-//     <label htmlFor="name">Herd Name</label>
-//     <input className='form-control' id="name" name="name" value={data.cattleHerd.name} required />
-//     { cattle }
-// </div>
-// </form>
-// {/* BUTTONS */}
-// {/* EDIT HERD BUTTON */}
-// <input className='btn btn-primary' type="submit" value="Edit Herd" />
-// {/* EDIT CATTLE BUTTON */}
-// <div>
-// <a href={`./${data.cattleHerd.id}/edit`} className='btn btn-success btn-lg' style={{marginRight: '15px'}}>
-// Edit
-// </a>
-// </div>
